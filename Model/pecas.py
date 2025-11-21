9b2815938c51338c698209f9337e8da77ef3573a
Bruna <140204799+brunahararj@users.noreply.github.com>
2025-11-21 19:54:21 -0300

    Código peças rodando

--- Model/pecas.py
+++ Model/pecas.py
@@ -12,17 +12,16 @@ df_csv = df_csv.drop(columns=colunas_para_remover, errors='ignore')
 
 df_csv['Ano'] = ANO
 
-df_csv['Data_Completa'] = (df_csv['Data'].astype(str) + '-' + df_csv['Mês'].astype(str) + '-' + df_csv['Ano'].astype(str))
+df_csv['Dia'] = pd.to_datetime(df_csv['Dia'], format='%d/%m/%Y', errors='coerce')
 
-# --- Conversão para Datetime com Formato Específico ---
-df_csv['Data_Reposicao'] = pd.to_datetime(df_csv['Data_Completa'],format='%d-%m-%Y',errors='coerce')
+df_csv['Data_Completa'] = df_csv['Dia'].dt.strftime('%d-%m-%Y')
 
-df_csv = df_csv.drop(columns=['Data', 'Mês', 'Ano', 'Data_Completa'], errors='ignore')
+df_csv = df_csv.drop(columns=['Dia', 'Mês', 'Ano', 'Data_Completa'], errors='ignore')
 
 
 # --- Exibição dos Resultados ---
 print("Processamento concluído com sucesso!")
-print(f"Data assumida para os registros: {ANO}")
+print(f"Dia assumida para os registros: {ANO}")
 print("\nDimensões do DataFrame:", df_csv.shape)
 print("\nPrimeiras 5 linhas (com a nova coluna 'Data_Reposicao'):")
 print(df_csv.head(300))
